6d9ac139a5d86e06f2a4d1ed07aa4c8db20fc05c
Matthias Rudin <matthias.rudin@live.com>
2019-03-02 08:58:26 +0100

    Add project route error handling

--- routes/projects.js
+++ routes/projects.js
@@ -6,8 +6,18 @@ const { projects } = require('../data.json');
 Routes
 ************************************************************************************/
 // Custom project page
-router.get('/:id',(req, res) => {
+router.get('/:id',(req, res,next) => {
     const id = req.params.id;
+
+    // If the project does not exist, thow a 404 error
+    if(projects[id] === undefined) {
+        const err = new Error('Page Not Found');
+        err.status = 404;
+        console.log(`${err}: ${err.status}`);
+        return next(err);
+    }
+
+    // If the project exist, display it with the pug layout
     res.render('project', {
         project: projects[id]
     });
